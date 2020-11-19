0960984f49d55c52c75a6c64409daf710c7bec29
liuchangda <1603491144@qq.com>
2020-11-19 19:51:52 +0800

    3

---
+++ sp03-userservice/src/test/java/cn/tedu/sp03/Sp03UserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.sp03;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp03UserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
